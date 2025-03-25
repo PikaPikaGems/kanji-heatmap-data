1b3e3a01f189299810bc2a25dce8af20400ce2f2
Mithi Sevilla <mithi@luxor.tech>
2025-03-26 04:52:27 +0800

    Add vocab overrides functionality and refactor

--- README.md
+++ README.md
@@ -13,12 +13,27 @@ python --version
 
 #### Input Files
 
+Download aggregated kanji information from [Kanji Data Releases][pika-data]
+
+```bash
+curl --output-dir input -OL https://github.com/PikaPikaGems/kanji-data-releases/releases/latest/download/kanji-data.tar.gz
+tar -xzf ./input/kanji-data.tar.gz -C ./input/
+```
+
+Download the map of vocabulary to its components from [JMdict Furigana Map][pika-furi]
+
 ```bash
 curl --output-dir input -OL https://github.com/PikaPikaGems/jmdict-furigana-map/releases/latest/download/jmdict-furigana-map.json.tar.gz
 tar -xzf ./input/jmdict-furigana-map.json.tar.gz -C ./input/
+```
 
-curl --output-dir input -OL https://github.com/PikaPikaGems/kanji-data-releases/releases/latest/download/kanji-data.tar.gz
-tar -xzf ./input/kanji-data.tar.gz -C ./input/
+Download and prepare the the Simplified JMdict JSON file
+
+```
+curl --output-dir input -OL https://github.com/scriptin/jmdict-simplified/releases/download/3.6.1%2B20250324123350/jmdict-eng-common-3.6.1+20250324123350.json.tgz
+tar -xzf ./input/jmdict-eng-common-3.6.1+20250324123350.json.tgz -C ./input/
+mv input/jmdict-eng-common-3.6.1.json input/scriptin-jmdict-eng-common.json
+
 ```
 
 ### Transform Data
@@ -36,14 +51,27 @@ The following output files should be generated in the `output` directory:
 - phonetic.json
 - vocabulary.json
 
-## License
+## License and Credits
 
 The software is distributed under the [MIT License][mit-license].
 
-The input data comes from [Kanji Data Releases][pika-data] and [JMdict Furigana Map][pika-furi],
-both under [CC BY-SA 4.0][cc-by-sa-4].
+The input data comes from:
+
+1. Dmitry Shpika's [jmdict-simplified](https://github.com/scriptin/jmdict-simplified) which project uses the [JMdict/EDICT][jmdict-edict] file, which is the property of the Electronic Dictionary Research and Development Group (https://www.edrdg.org/),
+   and used in conformance with the Group's [license](https://www.edrdg.org/edrdg/licence.html).
+2. [Kanji Data Releases][pika-data] and [JMdict Furigana Map][pika-furi],
+   both under [CC BY-SA 4.0][cc-by-sa-4].
+
+### JMdict and JMnedict
+
+The original XML files - **JMdict.xml**, **JMdict_e.xml**, **JMdict_e_examp.xml**,and **JMnedict.xml** -
+are the property of the Electronic Dictionary Research and Development Group,
+and are used in conformance with the Group's [license][EDRDG-license].
+All derived files are distributed under the same license, as the original license requires it.
 
 [mit-license]: https://github.com/PikaPikaGems/kanji-heatmap-data/blob/main/LICENSE
 [cc-by-sa-4]: https://creativecommons.org/licenses/by-sa/4.0
 [pika-data]: https://github.com/PikaPikaGems/kanji-data-releases
 [pika-furi]: https://github.com/PikaPikaGems/jmdict-furigana-map
+[EDRDG-license]: http://www.edrdg.org/edrdg/licence.html
+[jmdict-edict]: https://www.edrdg.org/wiki/index.php/JMdict-EDICT_Dictionary_Project
--- src/kanji_build_output_jsons.py
+++ src/kanji_build_output_jsons.py
@@ -3,37 +3,53 @@
 import kanji_extract
 import kanji_load
 
-NUMBER_DATA_NOT_FOUND_VAL = -1
+NO_NUM_DATA = -1
 
 kanji_main_reformatted = {}
 kanji_extended_reformatted = {}
-for kanji in kanji_load.KANJI_INFO["KANJI_LIST"]:
+all_words = set(())
+
+def get_words(kanji):
+    original_words = kanji_load.KANJI_INFO["KANJI_WORDS"].get(kanji, [])
+    override_words = kanji_load.KANJI_INFO["OWN_VOCAB_OVERRIDE"].get(kanji, [])
+    kanji_words = (override_words + original_words)[:2]
+    return kanji_words
+
+for kanji in kanji_load.KANJI_INFO["KANJI_DATA"].keys():
 
     kanji_info = kanji_load.KANJI_INFO["KANJI_DATA"][kanji]
-    
+
+    keyword_overrides = kanji_load.KANJI_INFO["OWN_KEYWORDS_OVERRIDE"]
     kanji_main_reformatted[kanji] = [
-        kanji_extract.get_keyword(kanji_info, kanji_load.KANJI_INFO["OWN_KEYWORDS_OVERRIDE"]) or "",
+        kanji_extract.get_keyword(kanji_info, keyword_overrides) or "",
         kanji_extract.get_main_on_reading(kanji_info) or "",
         kanji_extract.get_main_kun_reading(kanji_info) or "",
-        kanji_extract.get_jlpt(kanji_info) or NUMBER_DATA_NOT_FOUND_VAL,
-        kanji_extract.get_ranks(kanji_info, NUMBER_DATA_NOT_FOUND_VAL),
+        kanji_extract.get_jlpt(kanji_info) or NO_NUM_DATA,
+        kanji_extract.get_ranks(kanji_info, NO_NUM_DATA),
     ]
 
+    kanji_words = get_words(kanji)
+    all_words.update(kanji_words)
+
+    parts_overrides =  kanji_load.KANJI_INFO["OWN_PARTS_OVERRIDE"]
     kanji_extended_reformatted[kanji] = [
-        kanji_extract.get_component_parts(kanji_info, kanji_load.KANJI_INFO["OWN_PARTS_OVERRIDE"]) or [],
-        kanji_extract.get_strokes(kanji_info) or NUMBER_DATA_NOT_FOUND_VAL,
-        kanji_extract.get_rtk_index(kanji_info) or NUMBER_DATA_NOT_FOUND_VAL,
-        kanji_extract.get_wanikani_lvl(kanji_info) or NUMBER_DATA_NOT_FOUND_VAL,
-        kanji_extract.get_jouyou(kanji_info) or NUMBER_DATA_NOT_FOUND_VAL,
+        kanji_extract.get_component_parts(kanji_info, parts_overrides),
+        kanji_extract.get_strokes(kanji_info) or NO_NUM_DATA,
+        kanji_extract.get_rtk_index(kanji_info) or NO_NUM_DATA,
+        kanji_extract.get_wanikani_lvl(kanji_info) or NO_NUM_DATA,
+        kanji_extract.get_jouyou(kanji_info) or NO_NUM_DATA,
         kanji_extract.get_all_meanings(kanji_info) or [],
         kanji_extract.get_all_on_readings(kanji_info) or [],
         kanji_extract.get_all_kun_readings(kanji_info) or [],
         kanji_extract.get_semantic_phonetic(kanji_info) or [],
-        kanji_load.KANJI_INFO["KANJI_WORDS"].get(kanji, [])
+        kanji_words
     ]
 
 kanji_load.dump_to_main_kanji_info(kanji_main_reformatted)
 kanji_load.dump_to_extended_kanji_info(kanji_extended_reformatted)
-kanji_load.dump_created_info_lookups()
+kanji_load.dump_word_details(all_words)
+kanji_load.dump_phonetic_components()
+kanji_load.dump_cum_use()
+kanji_load.dump_part_keyword_with_overrides()
 
 print("Done.")
